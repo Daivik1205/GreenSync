472fc9f7a70f5ec026fab96fb0e9183b28a5365c
Daivik1205 <vikk.shenoy@gmail.com>
2026-03-21 16:03:25 +0530

    Initial phase

---
+++ greensync_phase1/main.py
@@ -0,0 +1,28 @@
+import traci
+import os
+import sys
+
+if 'SUMO_HOME' not in os.environ:
+    sys.exit("SUMO_HOME not set")
+
+sumoCmd = ["sumo-gui", "-c", "map.sumocfg"]
+traci.start(sumoCmd)
+
+step = 0
+
+while step < 200:
+    traci.simulationStep()
+
+    vehicle_ids = traci.vehicle.getIDList()
+
+    print(f"\n--- Step {step} ---")
+
+    for vid in vehicle_ids[:5]:  # limit output
+        pos = traci.vehicle.getPosition(vid)
+        speed = traci.vehicle.getSpeed(vid)
+
+        print(f"{vid} -> Pos: {pos}, Speed: {round(speed,2)}")
+
+    step += 1
+
+traci.close()
